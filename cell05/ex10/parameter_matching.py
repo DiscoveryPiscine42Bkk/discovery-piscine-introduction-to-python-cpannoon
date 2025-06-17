71611a52b04477c2d939aa2592f84aa892b6fbb3
cpannoon <chayadapannoon@gmail.com>
2025-06-17 07:11:51 +0000

    now

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,6 +1,8 @@
 import sys
+
 if len(sys.argv) != 2:
     print("none")
+
 else:
     parameter = sys.argv[1]
     user_input = input("What was the parameter? ")
@@ -8,3 +10,6 @@ else:
         print("Good job!")
     else:
         print("Nope, sorry...")
+
+if __name__ == "__main__":
+    main()
